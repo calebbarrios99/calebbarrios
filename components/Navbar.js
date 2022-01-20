765c454c231dacd645429468c6a2440b2c3ae67e
calebbarrios99 <cbarrios49@misena.edu.co>
2022-01-20 18:15:05 -0500

    +

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
 
 export default function Navbar() {
-  return <div>
-      Navbar
+  return <div className='navbar'>
+    <Menu secondary>
+
+    </Menu>
   </div>;
 }
